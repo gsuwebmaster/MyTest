33202084f188adede57e6c0eb6163d494d548d2b
Jesus <gsuweb21@gmail.com>
2021-10-08 20:13:34 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,264 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"-->
+    <title>Telcel Challenger</title>
+
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="imgSRC/icon.png" type="image/x-icon">
+
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <!--H&S style-->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <div class="section-one">    
+        <!-- banner menu -->
+        <div class="row fluid" style="margin: 0; padding: 0">
+            <div class="col-6 header-menu" style="margin: 0; padding: 0">
+                <div class="row fluid" style="margin: 0; padding: 0">
+                    <div class="col-6">
+                        <ul>
+                            <a href="#comoParticipar"><li>Cómo participar</li></a>
+                            <a href="#moviles"><li>Móviles</li></a>
+                            <a href="#ranking"><li>Ranking</li></a>
+                        </ul>
+                    </div>
+                    <div class="col-6 logo" style="margin: 0; padding: 0">
+                        <img src="img/Image 2.png" alt="Telcel Challenger logo">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <button class="buttonsOne" style=" margin-left: 70%">Comienza ahora</button>                
+            </div>
+        </div>
+        <!-- first content -->
+        <div class="row fluid" style="margin: 0; padding: 0">
+            <div class="col-6" style="margin: 0; padding: 0; position: relative"> 
+                <img style="position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%)" src="img/Group 1.svg" alt="">
+                <img class="img img-fluid" src="img/ScreenShotPM.png" alt="">
+            </div>
+            <div class="col-6" style="margin: auto auto; text-align: center">
+                <img src="img/Image1.png" alt="">
+                <h2><b>COMPRA, CONCURSA Y GANA</b></h2>
+                <button class="buttonsTwo">Conoce Cómo</button>  
+            </div>
+            <div class="col-5">
+
+                <div style="height: 150px"></div>
+
+                <div id="comoParticipar" style="position:relative">
+                <p class="special-title-one">Conoce Cómo</p>
+                <p class="special-title-two">PARTICIPAR</p>
+                </div>
+                <p class="text-content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel eius voluptatem porro libero velit natus amet quasi, tenetur incidunt ab, facere consectetur dolore consequatur aperiam pariatur, totam officiis quisquam saepe.
+                    <br>
+                    <br>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti aut consequatur veritatis mollitia earum soluta labore voluptatum corrupti assumenda animi, maiores aperiam, molestias quas expedita sit et! Reprehenderit, reiciendis exercitationem?
+                </p>
+                <button class="buttonsOne">Inscríbete</button>
+            </div>
+        </div>
+    </div>
+
+    <div id="moviles" class="section-two">  
+        <div class="row">
+            <div class="col-6" style="position: relative">
+                <img style="position: absolute; transform: translateY(-3%)" src="img/Image13.png" alt="">
+            </div>
+            <div class="col-6">
+                <table class="table-one">
+                    <th>
+                        <td colspan="4" style="text-alig, center">
+                            <div>
+                                <div style="position:relative; width: 300px">
+                                    <p class="special-title-three">Compra tu</p>
+                                    <p class="special-title-four">MÓVIL</p>
+                                </div>
+                            </div>
+                        </td>
+                    </th>
+                    <tr>
+                        <td style="width: 25%">
+                            <img src="img/Image6.png" alt="Logo">
+                        </td>
+                        <td style="width: 25%">
+                            <img src="img/Image7.png" alt="Logo">
+                        </td>
+                        <td style="width: 25%">
+                            <img src="img/Image8.png" alt="Logo">
+                        </td>
+                        <td style="width: 25%">
+                            <img src="img/Image9.png" alt="Logo">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="4" style="padding-top: 20px">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, laboriosam eveniet? Ipsa laudantium suscipit nulla soluta eveniet at a debitis fugit, facere nam. Quidem dolores eaque accusamus maiores repellendus provident!                        
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="4">
+                            <button class="buttonsThree">Comprarlo ahora</button>
+                        </td>                    
+                    </tr>
+                </table>        
+            </div>
+        </div>
+    </div>
+
+    <div class="section-three" id="ranking">
+        <div class="container-fluid" style="margin: 0; padding: 80px 0 0 0">
+            <div class="row fluid justify-content-around" style="margin: 0; padding: 0">
+                <div class="col-4" style="margin: 0; paddin 0; text-align: center; position: relative">
+                    <img style="position: absolute; left: 50%; transform: translateX(-50%); index: 10" src="img/Image16.png" alt="">
+                    <img class="fade1"  style="margin-top: 20px;" class="img img-fluid" src="img/Image14.png" alt="">
+                    <table class="table-two">
+                        <tr>
+                            <td>ULStriker2</td>
+                            <td>1801</td>
+                        </tr> 
+                        <tr>
+                            <td>SkillEZ001</td>
+                            <td>1788</td>
+                        </tr>  
+                        <tr>
+                            <td>GMX_</td>
+                            <td>1550</td>
+                        </tr>      
+                        <tr>
+                            <td>RandmHz</td>
+                            <td>1002</td>
+                        </tr>        
+                        <tr>
+                            <td>ROs3</td>
+                            <td>989</td>
+                        </tr> 
+                        <tr>
+                            <td>Arms0p3n8023</td>
+                            <td>901</td>
+                        </tr> 
+                        <tr>
+                            <td>Rockstarz578</td>
+                            <td>721</td>
+                        </tr>
+                        <tr>
+                            <td>CrazyOne</td>
+                            <td>607</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-4" style="margin: 0; padding: 0; text-align: center; position: relative">
+                    <img style="position: absolute; left: 50%; transform: translateX(-50%); index: 10" src="img/Image17.png" alt="">
+                    <img class="fade2"  style="margin-top: 20px" class="img img-fluid" src="img/Image15.png" alt="">
+                    <table class="table-two">
+                        <tr>
+                            <td>ULStriker2</td>
+                            <td>1801</td>
+                        </tr> 
+                        <tr>
+                            <td>SkillEZ001</td>
+                            <td>1788</td>
+                        </tr>  
+                        <tr>
+                            <td>GMX_</td>
+                            <td>1550</td>
+                        </tr>      
+                        <tr>
+                            <td>RandmHz</td>
+                            <td>1002</td>
+                        </tr>        
+                        <tr>
+                            <td>ROs3</td>
+                            <td>989</td>
+                        </tr> 
+                        <tr>
+                            <td>Arms0p3n8023</td>
+                            <td>901</td>
+                        </tr> 
+                        <tr>
+                            <td>Rockstarz578</td>
+                            <td>721</td>
+                        </tr>
+                        <tr>
+                            <td>CrazyOne</td>
+                            <td>607</td>
+                        </tr>
+                    </table>
+                </div>                
+                <div class="col-4" style="margin: 0; padding: 0; text-align: center; position: relative">
+                    <img style="position: absolute; left: 50%; transform: translateX(-50%); index: 10" src="img/Image18.png" alt="">
+                    <img class="fade3" style="margin-top: 20px" class="img img-fluid" src="img/Image14-1.png" alt="">
+                    <table class="table-two">
+                        <tr>
+                            <td>ULStriker2</td>
+                            <td>1801</td>
+                        </tr> 
+                        <tr>
+                            <td>SkillEZ001</td>
+                            <td>1788</td>
+                        </tr>  
+                        <tr>
+                            <td>GMX_</td>
+                            <td>1550</td>
+                        </tr>      
+                        <tr>
+                            <td>RandmHz</td>
+                            <td>1002</td>
+                        </tr>        
+                        <tr>
+                            <td>ROs3</td>
+                            <td>989</td>
+                        </tr> 
+                        <tr>
+                            <td>Arms0p3n8023</td>
+                            <td>901</td>
+                        </tr> 
+                        <tr>
+                            <td>Rockstarz578</td>
+                            <td>721</td>
+                        </tr>
+                        <tr>
+                            <td>CrazyOne</td>
+                            <td>607</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section-four">
+        <div class="container-fluid" style="margin: 0; padding: 0">
+            <div class="row fluid justify-content-center" style="margin: 0; padding: 0 20px">
+                <div class="col-4" style="padding: 0; margin: auto">
+                    <img src="img/Image 2.png" alt="">
+                </div>
+                <div class="col-4" style="padding: 0 0 0 10%; margin: auto; text-align: center">
+                    <table>
+                        <tr>
+                            <td style="padding: 0px 5px"><img src="img/Group31.svg" alt=""></td>
+                            <td style="padding: 0px 5px"><img src="img/Path3.svg" alt=""></td>
+                            <td style="padding: 0px 5px"><img src="img/Path4.svg" alt=""></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-4" style="padding: 0; margin: auto; text-align: right">
+                    <p style="color: white">Términos y Condiciones  Política de Datos</p>                    
+                </div>                                
+            </div>   
+        </div>     
+    </div>
+    
+    <script src="js/jquery.js"></script>
+</body>
+</html>
